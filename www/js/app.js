334a9b0293bd800cad52e071f8bc79e35c594491
emzan <zanettiemilio@hotmail.com>
2018-01-25 15:30:56 +0800

    small comment changes

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
+// angular.module is the best global place in the internet and software world for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
